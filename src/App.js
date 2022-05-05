a9f147fbd032e4d5ad351ba127e98dfa11fc4184
Suraj Thapa <stsurajst@gmail.com>
2022-05-05 21:43:43 +0545

    class based counter

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import "./App.css";
 import React from "react";
 
 class App extends React.Component {
-  constructor() {
+  constructor(props) {
     //honor the inheritance of the parent component by including the super() statement
-    super();
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increaseCounter() {
+    console.log("cc");
+    this.setState({ count: this.state.count + 1 });
   }
   render() {
     // class based component requires render method that returns HTML;
+
     return (
       <div className="App">
         <h3>Hello world</h3>
+        <h5>{this.state.count}</h5>
+        <button onClick={() => this.increaseCounter()}>Add</button>
       </div>
     );
   }
