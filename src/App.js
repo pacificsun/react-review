b1d4b868980b88d013cebc61627bf928a1231357
Suraj Thapa <stsurajst@gmail.com>
2022-05-05 21:19:22 +0545

    This is main for class based react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    //honor the inheritance of the parent component by including the super() statement
+    super();
+  }
+  render() {
+    // class based component requires render method that returns HTML;
+    return (
+      <div className="App">
+        <h3>Hello world</h3>
+      </div>
+    );
+  }
 }
+// To learn more check reference: https://www.w3schools.com/react/react_class.asp
 
 export default App;
